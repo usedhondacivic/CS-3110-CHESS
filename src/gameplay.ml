b68b1a099482244e727bb76170afd544ad0da921
Morgan Suh <mys37@cornell.edu>
2021-10-05 18:50:44 -0400

    fix error

--- src/gameplay.ml
+++ src/gameplay.ml
@@ -9,13 +9,16 @@ let check_file file =
   if file_num - 65 >= 0 && file_num - 65 < 25 then true else false
 
 (** check if rank in range*)
-let check_rank rank = 
-  let rank = int_of_string rank in 
-  if rank >= 1 && rank < 8 then true else false
+let check_rank rank_num = 
+  let rank_num = int_of_string rank_num in 
+  if rank_num >= 1 && rank_num < 8 then true else false
   
 let check_file_rank place = 
-  failwith "check_file_rank has not been implemented yet"
-
+  let file_place = String.sub place 0 1 in 
+  let rank_place = String.sub place 1 1 in 
+  if check_file file_place && check_rank rank_place
+    then failwith "new input needed"
+    else failwith "new input needed"
 
 let check start_end =
   (** change to return move*)
