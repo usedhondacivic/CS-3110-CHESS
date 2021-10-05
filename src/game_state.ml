9e791068ce4a003e0469f7368d1ef42c75e67814
Zoe De Simone <zd92@cornell.edu>
2021-10-04 23:19:13 -0400

    added piece type, move type and get_move to piece

--- src/game_state.ml
+++ src/game_state.ml
@@ -1,23 +1,28 @@
 type time = int * int
 
-type color = White | Black | Empty
+type color =
+  | White
+  | Black
+  | Empty
 
 type board = (Piece.piece * color) list list
 
 type board_coord = {
   rank : int;
-  file : int
+  file : int;
 }
 
 type game_state = {
   board : board;
-  current_turn: color;
+  current_turn : color;
   white_taken : Piece.piece list;
   black_taken : Piece.piece list;
-  time : time
+  time : time;
 }
-  
-type result = Legal | Illegal
+
+type result =
+  | Legal
+  | Illegal
 
 let game_over_check curr_board =
   failwith "attempt_move has not been implemented."
--- src/piece.ml
+++ src/piece.ml
@@ -1,1 +1,44 @@
-type piece =  Pawn | Rook | Bishop | King | Queen | Knight | Empty
+(* [type piece] represents possible chess pieces]. *)
+type piece =
+  | Pawn
+  | Rook
+  | Bishop
+  | King
+  | Queen
+  | Knight
+  | Empty
+
+exception UnknownPiece of piece
+
+(* [type direction] represents possible directions of a piece]. *)
+type direction =
+  | Vertical
+  | Horizontal
+  | Diagonal
+  | LShape
+
+(* [type move] represents a possible move]. *)
+type move = {
+  length : int;
+  directions : direction;
+}
+
+(* [type moves] represents possible moves of a piece]. *)
+type moves = {
+  p : piece;
+  validmove : move list;
+}
+
+let move_vert len dir = failwith "move_vert not implemented"
+
+let move_horiz len dir = failwith "move_horiz not implemented"
+
+let move_lshape len dir = failwith "move_lshape not implemented"
+
+let move_diag len dir = failwith "move_diag not implemented"
+
+let get_moves p = failwith "get_moves not implemented"
+
+(*evaluates to a position on the board givem a move length [len] and
+  direction [dir] *)
+let get_move len dir = failwith "get_move not implemented"
