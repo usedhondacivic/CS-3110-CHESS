d87036ee4412a5b8fadb7a04daf61cb5b37f8f07
Morgan Suh <mys37@cornell.edu>
2021-10-05 19:31:39 -0400

    added comments

--- src/gameplay.mli
+++ src/gameplay.mli
@@ -2,5 +2,7 @@ type move = {
   start : Game_state.board_coord;
   next : Game_state.board_coord;
 }
+(** The type of inputted move to show start and next square*)
 
 val check : string -> move
+(** Checks if inputted move is valid and returns move if valid*)
