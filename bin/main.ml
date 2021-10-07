7e2fb571d27666fb59291336343515e34a025bb7
Michael Crum <mmc323@cornell.edu>
2021-10-07 17:28:08 -0400

    Added test ANSITerminal code

--- bin/main.ml
+++ bin/main.ml
@@ -2,4 +2,8 @@
 Code to start gameplay will go here
 *)
 
-print_endline "bin/main.ml has yet to be implemented."
+(*print_endline "bin/main.ml has yet to be implemented."*)
+
+open ANSITerminal
+
+let _ = print_string [blue; on_green] "\n\n\n\ntest\n\n\n\n"
