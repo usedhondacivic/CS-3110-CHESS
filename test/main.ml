18203e1b2fb67120efb5392a9d7bb1c73dcbacd4
Morgan Suh <mys37@cornell.edu>
2021-10-16 12:44:31 -0400

    added test suite

--- test/main.ml
+++ test/main.ml
@@ -23,7 +23,11 @@ let get_start_test
     (expected_output : Game_state.board_coord) : test =
   name >:: fun _ -> assert_equal expected_output (get_start input)
 
-let gameplay_tests = failwith "gameplay_tests has not been implemented"
+let gameplay_tests =
+  [
+    check_test "'A4 A5' is on board" "A4 A6"
+      { start = { rank = 0; file = 4 }; next = { rank = 0; file = 6 } };
+  ]
 
 (** Construct OUnit tests for Move_validation*)
 let move_validation_tests =
@@ -34,3 +38,16 @@ let piece_tests = failwith "piece_tests has not been implemented"
 
 (** Construct OUnit tests for Ui*)
 let ui_tests = failwith "ui_tests has not been implemented"
+
+let suite =
+  "test suite for Chess"
+  >::: List.flatten
+         [
+           game_state_tests;
+           gameplay_tests;
+           move_validation_tests;
+           piece_tests;
+           ui_tests;
+         ]
+
+let _ = run_test_tt_main suite
