bb2e60d90025623168fd26521e562ee5a5fea5d7
Morgan Suh <mys37@cornell.edu>
2021-10-16 12:25:26 -0400

    opened modules in test

--- test/main.ml
+++ test/main.ml
@@ -1,1 +1,14 @@
 open OUnit2
+open Chess
+open Game_state
+open Gameplay
+open Move_validation
+open Piece
+open Ui
+
+(** Construct OUnit tests for Gameplay*)
+let check_test
+    (name : string)
+    (input : string)
+    (expected_output : Gameplay.move) : test =
+  name >:: fun _ -> assert_equal expected_output (check input)
