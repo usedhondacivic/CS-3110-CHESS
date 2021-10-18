e94d4514d076b121618f9876f052a2065175b800
Nicholas <nrb74@cornell.edu>
2021-10-18 17:02:43 -0400

    added result back in move validation

--- src/move_validation.mli
+++ src/move_validation.mli
@@ -1,8 +1,8 @@
-type result
-(**type result represents if a given board / move resulted in a legal
-   move*)
-
-val attempt_move : Game_state.board -> Game_state.board_coord -> Game_state.board_coord -> Game_state.board * result
+val attempt_move :
+  Game_state.board ->
+  Game_state.board_coord ->
+  Game_state.board_coord ->
+  Game_state.board * Game_state.result
 (**[attempt_move board board_coord board_coord] validates all board
    considerations (checks, blocked pieces, castling ect.) Returns
    [board, Legal] if the move is allowed*)
