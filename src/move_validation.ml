c3ab43da12a23494a3be2bba4de31b0a86ccdfab
Nicholas <nrb74@cornell.edu>
2021-10-18 17:03:06 -0400

    -

--- src/move_validation.ml
+++ src/move_validation.ml
@@ -1,11 +1,13 @@
-(**type result represents if a given board / move resulted in a legal
-   move*)
 type result =
   | Legal
   | Illegal
 
-(**[attempt_move board board_coord board_coord] validates all board
-   considerations (checks, blocked pieces, castling ect.) Returns
-   [board, Legal] if the move is allowed*)
-let attempt_move board board_coord board_coord =
-  failwith "attempt_move has not been implemented"
+let ll_to_aa ll = Array.of_list (List.map Array.of_list ll)
+
+let gboard_to_aboard gboard = ll_to_aa gboard
+
+let friendly_fire aboard start_coard finish_coard =
+  aboard.(start_coard.Game_state.rank).(start_coard.Game_state.file)
+  == aboard.(finish_coard.Game_state.rank).(finish_coard.Game_state.file)
+
+let attempt_move board start finish = board
