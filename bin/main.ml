9676b5fecd1f77ce60ef0a91e833435dedbb4ee7
Michael Crum <mmc323@cornell.edu>
2021-10-18 19:02:02 -0400

    Removed ANSITerminal

--- bin/main.ml
+++ bin/main.ml
@@ -3,12 +3,9 @@ Code to start gameplay will go here
 *)
 
 (*print_endline "bin/main.ml has yet to be implemented."*)
-
-open ANSITerminal
-
 open Chess
 
-let start_board = Game_state.get_board_from_FEN  "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR"
+let start_board = Game_state.get_board_from_FEN  "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 
 let start_state : Game_state.game_state = {
   board = start_board;
