ee30f58122446e64a637a2e61fe54471ea3d5370
Morgan Suh <mys37@cornell.edu>
2021-10-18 14:42:16 -0400

    fixed gameplay test and check function

--- test/main.ml
+++ test/main.ml
@@ -6,6 +6,14 @@ open Move_validation
 open Piece
 open Ui
 
+(**Print tuple for testing *)
+let print_tuples = function
+  | {
+      start = { rank = s_r; file = f_r };
+      next = { rank = n_r; file = n_f };
+    } ->
+      Printf.sprintf "%i, %i, %i, %i;" s_r f_r n_r n_f
+
 (** Construct OUnit tests for Game_state*)
 let game_state_tests = [ (* TODO: add your tests here *) ]
 
@@ -14,7 +22,8 @@ let check_test
     (name : string)
     (input : string)
     (expected_output : Gameplay.move) : test =
-  name >:: fun _ -> assert_equal expected_output (check input)
+  name >:: fun _ ->
+  assert_equal expected_output (check input) ~printer:print_tuples
 
 let get_start_test
     (name : string)
@@ -25,7 +34,7 @@ let get_start_test
 let gameplay_tests =
   [
     check_test "'A4 A5' is on board" "A4 A6"
-      { start = { rank = 0; file = 4 }; next = { rank = 0; file = 6 } };
+      { start = { rank = 4; file = 1 }; next = { rank = 6; file = 1 } };
   ]
 
 (** Construct OUnit tests for Move_validation*)
